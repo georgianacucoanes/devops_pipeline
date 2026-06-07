8c6457a2a8f9302b976ec6b26452f29f253722bd
Georgiana Cucoanes <georgiana.cucoanes@gmail.com>
2026-06-07 22:08:46 +0300

    Add new endpoints: time,tasks GET and POST

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # devops_pipeline
+Think of a flask application as a restaurant : 
+
+flask app= the kitchen 
+endpoint= the serving window(each window serves something different)
+request=your order
+response=what you get back
+
+Every time you type a URL in your browser , you re making a request to a specific endpoint, and the server sends back a response.
+
+Each @app.route(...) in the code defines one of those windows:
+/   -> "Hello, the server is running"
+/health  ->"Yes, I'm up an healthy"
+/time -> "Current time iss 14:32:05"
+/tasks -> "Here are your tasks" (GET) or "Task added!"(Post) . The difference between get and post is that get is used when you re asking for something and post is used when you're senfing something
--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
+from datetime import datetime
 
 app = Flask(__name__)
+tasks=[]
 
 @app.route("/")
 def home():
@@ -9,6 +11,24 @@ def home():
 @app.route("/health")
 def health():
     return jsonify({"status": "healthy"})
+
+@app.route("/time")
+def current_time():
+    now=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return jsonify({"current_time":now})
+
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+    return jsonify({"tasks":tasks})
+
+@app.route("/tasks",methods=['POST'])
+def add_task():
+    data=request.get_json()
+    task=data.get("task")
+    if not task:
+        return jsonify({"error": "No task provided"}),400 
+    tasks.append(task)
+    return jsonify({"message": "Task added", "task": task}), 201
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
