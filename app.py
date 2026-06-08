aef5f6ce49e0af355dac9992d56be9c7462ea977
Georgiana Cucoanes <georgiana.cucoanes@gmail.com>
2026-06-08 14:03:34 +0300

    Add task satus, delete and update endpoints with tests

--- app.py
+++ app.py
@@ -27,8 +27,31 @@ def add_task():
     task=data.get("task")
     if not task:
         return jsonify({"error": "No task provided"}),400 
-    tasks.append(task)
-    return jsonify({"message": "Task added", "task": task}), 201
+    new_task={
+        "id":len(tasks),
+        "task":task,
+        "status":"pending"
+    }
+    tasks.append(new_task)
+    return jsonify({"message": "Task added", "task": new_task}), 201
+
+@app.route("/tasks/<int:task_id>", methods=["PATCH"])
+def update_task_status(task_id):
+    if task_id >=len(tasks):
+        return jsonify({"error": "Task not found"}), 404 
+    data = request.get_json()
+    status = data.get("status")
+    if status not in ["pending", "done"]:
+        return jsonify({"error": "Status must be 'pending' or 'done'"}), 400
+    tasks[task_id]["status"] = status
+    return jsonify({"message": "Task updated", "task": tasks[task_id]})
+
+@app.route("/tasks/<int:task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    if task_id >= len(tasks):
+        return jsonify({"error": "Task not found"}), 404
+    deleted = tasks.pop(task_id)
+    return jsonify({"message": "Task deleted", "task": deleted})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- test_app.py
+++ test_app.py
@@ -1,9 +1,10 @@
 import pytest
-from app import app
+from app import app, tasks
 
 @pytest.fixture
 def client():
     app.config["TESTING"] = True
+    tasks.clear()
     with app.test_client() as client:
         yield client
 
@@ -16,3 +17,31 @@ def test_health(client):
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json["status"] == "healthy"
+
+def test_time(client):
+    response = client.get("/time")
+    assert response.status_code == 200
+    assert "current_time" in response.json
+
+def test_add_task(client):
+    response = client.post("/tasks", json={"task": "Learn DevOps"})
+    assert response.status_code == 201
+    assert response.json["task"]["status"] == "pending"
+
+def test_get_tasks(client):
+    client.post("/tasks", json={"task": "Learn DevOps"})
+    response = client.get("/tasks")
+    assert response.status_code == 200
+    assert len(response.json["tasks"]) == 1
+
+def test_update_task(client):
+    client.post("/tasks", json={"task": "Learn DevOps"})
+    response = client.patch("/tasks/0", json={"status": "done"})
+    assert response.status_code == 200
+    assert response.json["task"]["status"] == "done"
+
+def test_delete_task(client):
+    client.post("/tasks", json={"task": "Learn DevOps"})
+    response = client.delete("/tasks/0")
+    assert response.status_code == 200
+    assert response.json["task"]["task"] == "Learn DevOps"
