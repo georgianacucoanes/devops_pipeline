b064ed7ca69cd4a5682da30216c5eb22c6b2fd6e
Georgiana Cucoanes <georgiana.cucoanes@gmail.com>
2026-06-08 14:25:55 +0300

    Add evironment variables with python-dotenv

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def delete_task(task_id):
     return jsonify({"message": "Task deleted", "task": deleted})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=PORT,debug=DEBUG)
